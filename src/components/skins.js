73a435d78b539f8bacc30bdbb30da355374b63f8
Peder Johnsen <arnjeir+github@gmail.com>
2018-05-16 11:40:44 +0100

    Port <Skins /> component to native :tada:

--- src/components/skins.js
+++ src/components/skins.js
@@ -1,6 +1,62 @@
-// TODO: Port to native!
 import React from 'react'
 import PropTypes from 'prop-types'
+import { StyleSheet, View, TouchableWithoutFeedback } from 'react-native'
+
+const styles = StyleSheet.create({
+  skinSwatches: {
+    paddingTop: 2,
+    paddingBottom: 2,
+    borderWidth: 1,
+    borderColor: '#d9d9d9',
+    borderRadius: 14,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  skinSwatch: {
+    width: 0,
+    height: 0,
+    overflow: 'hidden',
+  },
+  skinSwatchShown: {
+    width: 20,
+    height: 16,
+    paddingLeft: 2,
+    paddingRight: 2,
+  },
+  skin: {
+    width: 16,
+    height: 16,
+    borderRadius: 32,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  skinSelected: {
+    width: 6,
+    height: 6,
+    borderRadius: 12,
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+  },
+  skinTone1: {
+    backgroundColor: '#ffc93a',
+  },
+  skinTone2: {
+    backgroundColor: '#fadcbc',
+  },
+  skinTone3: {
+    backgroundColor: '#e0bb95',
+  },
+  skinTone4: {
+    backgroundColor: '#bf8f68',
+  },
+  skinTone5: {
+    backgroundColor: '#9b643d',
+  },
+  skinTone6: {
+    backgroundColor: '#594539',
+  },
+})
 
 export default class Skins extends React.PureComponent {
   constructor(props) {
@@ -9,12 +65,9 @@ export default class Skins extends React.PureComponent {
     this.state = {
       opened: false,
     }
-
-    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick(e) {
-    var skin = parseInt(e.currentTarget.getAttribute('data-skin'))
+  handlePress(skin) {
     var { onChange } = this.props
 
     if (!this.state.opened) {
@@ -38,32 +91,30 @@ export default class Skins extends React.PureComponent {
       const selected = skinTone == skin
 
       skinToneNodes.push(
-        <span
+        <View
           key={`skin-tone-${skinTone}`}
-          className={`emoji-mart-skin-swatch ${
-            selected ? 'emoji-mart-skin-swatch-selected' : ''
-          }`}
+          style={[
+            styles.skinSwatch,
+            selected || opened ? styles.skinSwatchShown : null,
+          ]}
         >
-          <span
-            onClick={this.handleClick}
-            data-skin={skinTone}
-            className={`emoji-mart-skin emoji-mart-skin-tone-${skinTone}`}
-          />
-        </span>,
+          {selected || opened ? (
+            <TouchableWithoutFeedback
+              onPress={this.handlePress.bind(this, skinTone)}
+              style={[styles.skin, styles[`skinTone${skinTone}`]]}
+            >
+              <View style={[styles.skin, styles[`skinTone${skinTone}`]]}>
+                {selected && opened ? (
+                  <View style={styles.skinSelected} />
+                ) : null}
+              </View>
+            </TouchableWithoutFeedback>
+          ) : null}
+        </View>,
       )
     }
 
-    return (
-      <div>
-        <div
-          className={`emoji-mart-skin-swatches ${
-            opened ? 'emoji-mart-skin-swatches-opened' : ''
-          }`}
-        >
-          {skinToneNodes}
-        </div>
-      </div>
-    )
+    return <View style={styles.skinSwatches}>{skinToneNodes}</View>
   }
 }
 
