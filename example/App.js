5a24ac31bd06c1765fb666ede61ed4cfb7404a5f
ex7r3me <siavash.ahmadpour@gmail.com>
2018-09-22 12:22:59 +0330

    Add Picker and Toast to Example
    - add simple picker 
    - add toast for emoji `onSelect`

--- example/App.js
+++ example/App.js
@@ -11,28 +11,19 @@ import {
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Picker } from 'emoji-mart-native';
+import Toast, {DURATION} from 'react-native-easy-toast';
 
 type Props = {};
 export default class App extends Component<Props> {
+  addEmoji = (emoji) => {
+     this.refs.toast.show(emoji.id);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Picker set='emojione' onSelect={this.addEmoji} />
+        <Toast ref="toast"/>
       </View>
     );
   }
