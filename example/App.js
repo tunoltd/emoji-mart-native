7a1d9fb31eaf67c64764b66ecd1418a4ce97b67e
Peder Johnsen <me@pederjohnsen.com>
2018-11-13 20:18:15 +0000

    Use simple buttons to change emoji set.

--- example/App.js
+++ example/App.js
@@ -5,7 +5,7 @@
  */
 
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import {
   Emoji,
   NimblePicker,
@@ -14,13 +14,12 @@ import {
 } from 'emoji-mart-native'
 import data from 'emoji-mart-native/data/all.json'
 import dataRequires from 'emoji-mart-native/data/local-images/all'
-import { SetPicker } from './components'
 
 const { emojis: localEmojis } = dataRequires
 export default class App extends Component {
   state = {
     set: 'twitter',
-    selectedEmoji: { id: 'santa' },
+    selectedEmoji: { id: 'department_store' },
     showEmojiPicker: false,
   }
   emojiSelectTrigger = (emoji) => {
@@ -30,36 +29,33 @@ export default class App extends Component {
   showPickerTrigger = (visible) => {
     this.setState({ modalVisible: visible })
   }
+
+  onPressApple = () => {
+    this.setState({ set: 'apple' })
+  }
+
+  onPressGoogle = () => {
+    this.setState({ set: 'google' })
+  }
+
+  onPressTwitter = () => {
+    this.setState({ set: 'twitter' })
+  }
+
+  onPressMessenger = () => {
+    this.setState({ set: 'messenger' })
+  }
+
+  onPressFacebook = () => {
+    this.setState({ set: 'facebook' })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.titleContainer}>
           <Text style={styles.titleText}>Emoji Mart Native</Text>
-          <Emoji emoji="department_store" size={16} />
         </View>
-        <SetPicker
-          selectedValue={this.state.set}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({
-              set: itemValue,
-              showEmojiPicker: true,
-            })
-          }
-        />
-        <ModalPicker
-          isVisible={this.state.modalVisible}
-          showCloseButton={true}
-          onPressClose={() => {
-            this.showPickerTrigger(false)
-          }}
-          set={this.state.set}
-          data={data}
-          onSelect={(emoji) => {
-            this.emojiSelectTrigger(emoji)
-            this.showPickerTrigger(false)
-          }}
-          useLocalImages={localEmojis}
-        />
         <View style={styles.previewContainer}>
           <Emoji
             emoji={this.state.selectedEmoji}
@@ -71,6 +67,23 @@ export default class App extends Component {
             }}
           />
           <Text>{this.state.selectedEmoji.id}</Text>
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={[styles.btn, this.state.set === 'apple' ? styles.btnActive : null]} onPress={this.onPressApple}>
+            <Text>apple</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, this.state.set === 'google' ? styles.btnActive : null]} onPress={this.onPressGoogle}>
+            <Text>google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, this.state.set === 'twitter' ? styles.btnActive : null]} onPress={this.onPressTwitter}>
+            <Text>twitter</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, this.state.set === 'messenger' ? styles.btnActive : null]} onPress={this.onPressMessenger}>
+            <Text>messenger</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, this.state.set === 'facebook' ? styles.btnActive : null]} onPress={this.onPressFacebook}>
+            <Text>facebook</Text>
+          </TouchableOpacity>
         </View>
         <NimblePicker
           set={this.state.set}
@@ -86,6 +99,20 @@ export default class App extends Component {
             }}
           />
         </View>
+        <ModalPicker
+          isVisible={this.state.modalVisible}
+          showCloseButton={true}
+          onPressClose={() => {
+            this.showPickerTrigger(false)
+          }}
+          set={this.state.set}
+          data={data}
+          onSelect={(emoji) => {
+            this.emojiSelectTrigger(emoji)
+            this.showPickerTrigger(false)
+          }}
+          useLocalImages={localEmojis}
+        />
       </View>
     )
   }
@@ -98,16 +125,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
     paddingTop: 25,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
   titleContainer: {
     flexDirection: 'row',
@@ -123,5 +140,29 @@ const styles = StyleSheet.create({
   openModalText: {
     flexDirection: 'row',
     marginTop: 15,
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  btn: {
+    borderWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    borderRadius: 5,
+    paddingTop: 4,
+    paddingBottom: 4,
+    paddingLeft: 6,
+    paddingRight: 6,
+    marginRight: 5,
+    marginBottom: 5,
+  },
+  btnActive: {
+    borderColor: '#ae65c5',
+  },
+  btnText: {
+    fontSize: 24,
   },
 })
