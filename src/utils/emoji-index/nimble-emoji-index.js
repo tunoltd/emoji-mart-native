4676c32d2ae913a19499d1aa6f310c5a452f987c
Peder Johnsen <arnjeir+github@gmail.com>
2019-06-30 17:37:53 +0100

    Missed one

--- src/utils/emoji-index/nimble-emoji-index.js
+++ src/utils/emoji-index/nimble-emoji-index.js
@@ -164,7 +164,7 @@ export default class NimbleEmojiIndex {
                 } else {
                   return aScore - bScore
                 }
-              });
+              })
 
             }
 
