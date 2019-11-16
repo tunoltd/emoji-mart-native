069ab2899876f44a7b1470b80900fb82f56bef88
Peder Johnsen <me@pederjohnsen.com>
2019-11-16 16:39:36 +0000

    Remove dist files from git

--- dist/polyfills/classCallCheck.js
+++
@@ -1,1 +0,0 @@
-'use strict';Object.defineProperty(exports,"__esModule",{value:true});exports.default=function(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError('Cannot call a class as a function');}};
